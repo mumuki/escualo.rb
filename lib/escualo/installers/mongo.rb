5fe11984d4eaf8f99a7454490d3c7e5c0590eae3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-10-26 19:58:42 -0300

    Fixing some checks in mongo

--- lib/escualo/installers/mongo.rb
+++ lib/escualo/installers/mongo.rb
@@ -4,14 +4,14 @@ module Escualo::Installers
       ssh.exec! %Q{
         echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list && \
         apt-get update && \
-        apt-get install -y mongodb-org && \
+        apt-get install -y --force-yes mongodb-org && \
         echo '' >> /etc/init/mongodb && \
-        echo 'respawn' >> /etc/init/mongodb"
+        echo 'respawn' >> /etc/init/mongodb
       }
     end
 
     def check(ssh)
-      ssh.exec!("mongod --version").include? "db version"
+      ssh.exec!("mongod --version").start_with? 'db version v3.2'
     end
   end
 end
