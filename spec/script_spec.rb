810468bd6f9a6218a73b2a744dbb7b4dd7627863
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-08 19:14:06 -0300

    Improving script spec

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -4,7 +4,10 @@ describe 'escualo script' do
   it 'can run simple scripts', if: vagrant_up do
     result = escualo 'script data/atheneum.yml'
     expect($?).to eq 0
-    expect(result).to include 'Script executed successfully'
+
+    expect(ssh 'ruby --version').to include 'ruby 2.0'
+    expect(ssh 'psql --version').to include 'PostgreSQL'
+    expect(ssh 'service atheneum status').to include 'service atheneum running'
   end
 end
 
