bbb3f9bc35e389b7ef63f3d74fd652fb58621665
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-08 12:46:13 -0300

    Fixing some tests

--- spec/env_spec.rb
+++ spec/env_spec.rb
@@ -4,12 +4,14 @@ describe 'escualo env' do
   it { expect(Escualo::Env.locale_variables).to eq 'LANG' => 'en_US.UTF-8',
                                                    'LC_ADDRESS' => 'en_US.UTF-8',
                                                    'LC_ALL' => 'en_US.UTF-8',
+                                                   'LC_IDENTIFICATION' => 'en_US.UTF-8',
                                                    'LC_MEASUREMENT' => 'en_US.UTF-8',
                                                    'LC_MONETARY' => 'en_US.UTF-8',
                                                    'LC_NAME' => 'en_US.UTF-8',
                                                    'LC_NUMERIC' => 'en_US.UTF-8',
                                                    'LC_PAPER' => 'en_US.UTF-8',
-                                                   'LC_TELEPHONE' => 'en_US.UTF-8' }
+                                                   'LC_TELEPHONE' => 'en_US.UTF-8',
+                                                   'LC_TIME' => 'en_US.UTF-8'}
   it { expect(Escualo::Env.production_variables).to eq 'NODE_ENV' => 'production',
                                                        'RACK_ENV' => 'production',
                                                        'RAILS_ENV' => 'production' }
