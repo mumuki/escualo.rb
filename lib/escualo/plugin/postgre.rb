7a04ca8fa6aa4af6a9bdd3ab40aae95b339879d8
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-08 12:30:41 -0300

    Improving postgre checks

--- lib/escualo/plugin/postgre.rb
+++ lib/escualo/plugin/postgre.rb
@@ -4,8 +4,7 @@ module Escualo::Plugin
       pg_hba_conf = '/etc/postgresql/9.3/main/pg_hba.conf'
 
       ssh.shell.perform! %Q{
-        apt-get install postgresql libpq-dev -y && \
-                                                            \
+        apt-get install postgresql libpq-dev -y &&
         echo 'local   all             postgres                                peer' > #{pg_hba_conf} && \
         echo 'local   all             postgres                                peer' >> #{pg_hba_conf} && \
         echo 'local   all             all                                     password' >> #{pg_hba_conf} && \
@@ -14,7 +13,7 @@ module Escualo::Plugin
     end
 
     def check(ssh)
-      ssh.shell.exec!('psql --version').include? 'psql (PostgreSQL) 9.3'
+      ssh.shell.exec!('psql').include? 'FATAL:  role "root" does not exist'
     end
   end
 end
