4ff4d1cf79f91d93dfe191e46e625920905a27e7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-09 03:11:19 -0300

    Creating postgre user within postgre plugin

--- lib/escualo/plugin/postgre.rb
+++ lib/escualo/plugin/postgre.rb
@@ -5,10 +5,14 @@ module Escualo::Plugin
 
       ssh.shell.perform! %Q{
         apt-get install postgresql libpq-dev -y &&
-        echo 'local   all             postgres                                peer' > #{pg_hba_conf} && \
-        echo 'local   all             postgres                                peer' >> #{pg_hba_conf} && \
-        echo 'local   all             all                                     password' >> #{pg_hba_conf} && \
-        echo 'host    all             all             127.0.0.1/32            md5' >> #{pg_hba_conf}
+        echo 'local   all             postgres                                peer' > #{pg_hba_conf} &&
+        echo 'local   all             postgres                                peer' >> #{pg_hba_conf} &&
+        echo 'local   all             all                                     password' >> #{pg_hba_conf} &&
+        echo 'host    all             all             127.0.0.1/32            md5' >> #{pg_hba_conf} &&
+        cd / &&
+        sudo -u postgres PGDATABASE='' psql <<EOF
+        create role \\$POSTGRESQL_DB_USERNAME with createdb login password '\\$POSTGRESQL_DB_PASSWORD';
+EOF
       }, options
     end
 
