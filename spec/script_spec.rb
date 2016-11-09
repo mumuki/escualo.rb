55595b016a9eacb1fd38b4e7e4840e4c33c279e2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-09 03:17:04 -0300

    Fixing script path

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'escualo script' do
   it 'can run simple scripts', if: vagrant_up do
-    result = escualo 'script data/atheneum.yml'
+    result = escualo 'script spec/data/atheneum.yml'
     expect($?).to eq 0
 
     expect(ssh 'ruby --version').to include 'ruby 2.0'
