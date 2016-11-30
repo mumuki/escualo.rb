b56800ee572ac03f838cf7b93310a6a54676c2f6
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-30 19:16:37 -0300

    Removing sample scripts.
    Moving to https://github.com/mumuki/mumuki-escualo-sample-scripts

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -22,38 +22,13 @@ describe 'escualo script' do
   end
 
   it 'can run simple atheneum server script', if: vagrant_up do
-    result = escualo 'script samples/server.simple.atheneum.yml'
+    result = escualo 'script spec/data/sample.script.yml'
     expect($?).to eq 0
 
     expect(ssh 'ruby --version').to include 'ruby 2.0'
     expect(ssh 'psql --version').to include 'PostgreSQL'
 
     expect(result).to_not include 'ERROR'
-
-    expect(ssh 'service atheneum status').to include 'atheneum start/running'
-
-  end
-
-  it 'can run development platform script', if: vagrant_up do
-    result = escualo 'script samples/development.platform.yml'
-    expect($?).to eq 0
-
-    expect(ssh 'ruby --version').to include 'ruby 2.0'
-    expect(ssh 'psql --version').to include 'PostgreSQL'
-    expect(ssh 'mongod --version').to include 'db version'
-
-    expect(result).to_not include 'ERROR'
-  end
-
-  it 'can run desktop atheneum script', if: vagrant_up do
-    result = escualo 'script samples/desktop.atheneum.yml'
-    expect($?).to eq 0
-
-    expect(ssh 'ruby --version').to include 'ruby 2.0'
-    expect(ssh 'psql --version').to include 'PostgreSQL'
-
-    expect(result).to_not include 'ERROR'
-
     expect(ssh 'service atheneum status').to include 'atheneum start/running'
   end
 end
