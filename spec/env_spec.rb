586d3b9ca8d4422fdfcc5158662e51a90b871bf9
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-12-05 15:51:04 -0300

    Renamed integration tests

--- spec/env_spec.rb
+++ spec/env_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'escualo env' do
+describe Escualo::Env do
   it { expect(Escualo::Env.locale_variables).to eq 'LANG' => 'en_US.UTF-8',
                                                    'LC_ADDRESS' => 'en_US.UTF-8',
                                                    'LC_ALL' => 'en_US.UTF-8',
@@ -15,44 +15,4 @@ describe 'escualo env' do
   it { expect(Escualo::Env.environment_variables('production')).to eq 'NODE_ENV' => 'production',
                                                                       'RACK_ENV' => 'production',
                                                                       'RAILS_ENV' => 'production' }
-  context 'on bootstraped env', if: vagrant_up do
-    before { escualo 'bootstrap --monit-password sample' }
-
-    describe 'env' do
-      before { escualo 'env clean' }
-
-      it 'can list env' do
-        result = escualo 'env list'
-
-        expect($?).to eq 0
-        expect(result).to include 'LANG=en_US.UTF-8'
-        expect(result).to include 'RACK_ENV=production'
-        expect(result).to include 'ESCUALO_BASE_VERSION'
-        expect(result).to_not include 'FOO'
-      end
-
-      it 'can set env' do
-        escualo 'env set FOO=BAR'
-        result = escualo 'env list'
-
-        expect($?).to eq 0
-        expect(result).to include 'FOO=BAR'
-      end
-
-      it 'can unset env' do
-        escualo 'env set FOO=BAR'
-        escualo 'env set BAZ=GOO'
-        escualo 'env set BAZINGA=ZAZ'
-
-        escualo 'env unset BAZINGA BAZ'
-
-        result = escualo 'env list'
-
-        expect($?).to eq 0
-        expect(result).to include 'FOO'
-        expect(result).to_not include 'BAZ'
-        expect(result).to_not include 'BAZINGA'
-      end
-    end
-  end
 end
