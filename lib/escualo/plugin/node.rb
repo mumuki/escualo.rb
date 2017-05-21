9d8430f8e7a48c3ce269d123c0e48af639b20c02
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-05-21 17:33:41 -0300

    Using . instead of source
    For better compatibility

--- lib/escualo/plugin/node.rb
+++ lib/escualo/plugin/node.rb
@@ -2,7 +2,7 @@ module Escualo::Plugin
   class Node
     def run(session, _options)
       session.tell_all! 'curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash',
-                        'source ~/.bashrc',
+                        '. ~/.bashrc',
                         'nvm install 4.2.4'
     end
 
