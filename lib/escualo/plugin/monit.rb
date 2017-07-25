0bb63d57d0201a419edcef1be304ce2209d2cb06
Franco Bulgarelli <franco@mumuki.org>
2017-07-25 16:12:31 -0300

    Deleting /etc/monitrc if present

--- lib/escualo/plugin/monit.rb
+++ lib/escualo/plugin/monit.rb
@@ -8,6 +8,7 @@ module Escualo::Plugin
                         "wget https://mmonit.com/monit/dist/binary/5.16/monit-#{options.monit_version}-linux-x64.tar.gz",
                         "tar -xzf monit-#{options.monit_version}-linux-x64.tar.gz",
                         "cp monit-#{options.monit_version}/bin/monit /usr/bin/monit",
+                        'rm -rf /etc/monitrc',
                         'ln -s /etc/monit/monitrc /etc/monitrc',
                         'service monit start',
                         "echo 'set httpd port 2812 and' > /etc/monit/conf.d/web-server",
